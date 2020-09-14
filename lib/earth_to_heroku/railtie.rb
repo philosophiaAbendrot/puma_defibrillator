4f4e91684cdb0ff46664daac7fdb82fd909857d7
philosophia <tonytaesung.ha@gmail.com>
2020-09-13 18:42:15 -0700

    Added a require to load RackTimeoutNet middleware file into Railtie.

--- lib/earth_to_heroku/railtie.rb
+++ lib/earth_to_heroku/railtie.rb
@@ -1,6 +1,8 @@
 module EarthToHeroku
   class Railtie < ::Rails::Railtie
     initializer 'earthtoheroku.middleware.rails' do |app|
+      require 'earth_to_heroku/rack_timeout_net'
+
       app.config.middleware.insert_before ActionDispatch::ShowExceptions,
                                           EarthToHeroku::RackTimeoutNet
     end
