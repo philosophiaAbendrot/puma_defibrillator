020b404c04730b987197d80cbd38f84b829837b4
philosophia <tonytaesung.ha@gmail.com>
2020-09-13 18:37:52 -0700

    Slight syntax change

--- lib/earth_to_heroku/railtie.rb
+++ lib/earth_to_heroku/railtie.rb
@@ -1,5 +1,5 @@
 module EarthToHeroku
-  class RailTie < ::Rails::Railtie
+  class Railtie < ::Rails::Railtie
     initializer 'earthtoheroku.middleware.rails' do |app|
       app.config.middleware.insert_before ActionDispatch::ShowExceptions,
                                           EarthToHeroku::RackTimeoutNet
