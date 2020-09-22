11d1e8a7fd1211766a5ca875c32ee8588bd3374f
Tony Ha <tonytaesung.ha@gmail.com>
2020-09-21 21:47:51 -0700

    Changed order of middleware insertion to after ActionDispatch::ShowExceptions.

--- lib/earth_to_heroku/railtie.rb
+++ lib/earth_to_heroku/railtie.rb
@@ -3,7 +3,7 @@ module EarthToHeroku
     initializer 'earthtoheroku.middleware.rails' do |app|
       require 'earth_to_heroku/rack_timeout_net'
 
-      app.config.middleware.insert_after Rack::Timeout,
+      app.config.middleware.insert_after ActionDispatch::ShowExceptions,
                                          RackTimeoutNet
     end
   end
