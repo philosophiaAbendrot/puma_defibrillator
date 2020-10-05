ce2a44573d58ec3be8e643086a9df7ec87aa9245
Tony Ha <tonytaesung.ha@gmail.com>
2020-10-04 20:48:21 -0700

    simplified show_exception override logic. Added new debugging printouts.

--- lib/puma_defibrillator/show_exceptions.rb
+++ lib/puma_defibrillator/show_exceptions.rb
@@ -1,39 +1,25 @@
 module PumaDefibrillator
 	module ShowExceptions
-		def render_exception_with_puma_defib(env, exception)
-			key = 'action_dispatch.show_detailed_exceptions'
-
-			puts "===================================="
-			puts "triggered render_exception_with_puma_defib"
-
-			if exception.is_a?(ActionController::RoutingError) && env[key]
-				puts "caught exception in puma defibrillator"
-			end
-
-			render_exception_without_puma_defib(env, exception)
-		end
-
 		def call_with_puma_defib(env)
-			begin
-				puts "======================================="
-				puts "call_with_puma_defib checkpoint 1"
-				result = call_without_puma_defib(env)
-				puts "call_with_puma_defib checkpoint 2"
-			rescue ActionController::RoutingError => exception
-				scope = extract_scope_from(env)
-				puts "triggered call_with_puma_defib checkpoint 3"
-				raise exception
+			puts "=========================================="
+			puts "call with puma_defib checkpoint 1"
+			@app.call(env)
+		rescue Exception => exception
+			if exception.is_a?(Rack::Timeout::RequestTimeoutException)
+				puts "call with puma_defib checkpoint 2"
 			end
 
-			puts "call_with_puma_defib checkpoint 3"
-			result
+			if env['action_dispatch.show_exceptions'] == false
+				puts "call with puma_defib checkpoint 3"
+        raise exception
+      else
+      	puts "call_with_puma_defib checkpoint 4"
+        render_exception(env, exception)
+      end
 		end
 
 		def self.included(base)
-			base.send(:alias_method, :call_without_puma_defib, :call)
 			base.send(:alias_method, :call, :call_with_puma_defib)
-			base.send(:alias_method, :render_exception_without_puma_defib, :render_exception)
-			base.send(:alias_method, :render_exception, :render_exception_with_puma_defib)
 		end
 	end
 end
