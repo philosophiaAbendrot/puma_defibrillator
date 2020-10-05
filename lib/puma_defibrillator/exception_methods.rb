76dbf1e938e72f98fa987cf000b9cb0f937846f4
Tony Ha <tonytaesung.ha@gmail.com>
2020-10-04 22:18:25 -0700

    Wrapped exception handling logic inside included block.

--- lib/puma_defibrillator/exception_methods.rb
+++ lib/puma_defibrillator/exception_methods.rb
@@ -1,13 +1,13 @@
 module PumaDefibrillator
 	module ExceptionMethods
-		extend ActiveSupport::Concern
-		
-		rescue_from Rack::Timeout::RequestTimeoutException, with: :handle_rack_timeout
+		included do
+			rescue_from Rack::Timeout::RequestTimeoutException, with: :handle_rack_timeout
 
-		def handle_rack_timeout
-			puts "============================"
-      puts "log-rack-timeout running"
-      puts "============================"
+			def handle_rack_timeout
+				puts "============================"
+	      puts "log-rack-timeout running"
+	      puts "============================"
+			end
 		end
 	end
 end
