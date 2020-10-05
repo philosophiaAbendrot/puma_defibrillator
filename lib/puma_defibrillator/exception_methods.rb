bbde18d5ceff6b0284af52a12e337d9059be33a6
Tony Ha <tonytaesung.ha@gmail.com>
2020-10-04 22:15:25 -0700

    Extended 'rescue_from' method into Exception methods.

--- lib/puma_defibrillator/exception_methods.rb
+++ lib/puma_defibrillator/exception_methods.rb
@@ -1,5 +1,7 @@
 module PumaDefibrillator
 	module ExceptionMethods
+		extend ActiveSupport::Concern
+		
 		rescue_from Rack::Timeout::RequestTimeoutException, with: :handle_rack_timeout
 
 		def handle_rack_timeout
