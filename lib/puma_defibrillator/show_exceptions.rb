392c5a9f223089366c7e1da768fd15f5aa3db2f9
Tony Ha <tonytaesung.ha@gmail.com>
2020-10-04 19:51:00 -0700

    Test fix to ShowExceptions along with debugging printouts.

--- lib/puma_defibrillator/show_exceptions.rb
+++ lib/puma_defibrillator/show_exceptions.rb
@@ -3,8 +3,10 @@ module PumaDefibrillator
 		def render_exception_with_puma_defib(env, exception)
 			key = 'action_dispatch.show_detailed_exceptions'
 
+			puts "===================================="
+			puts "triggered render_exception_with_puma_defib"
+
 			if exception.is_a?(ActionController::RoutingError) && env[key]
-				puts "===================================="
 				puts "caught exception in puma defibrillator"
 			end
 
@@ -15,14 +17,16 @@ module PumaDefibrillator
 			call_without_puma_defib(env)
 		rescue ActionController::RoutingError => exception
 			scope = extract_scope_from(env)
+			puts "================================="
+			puts "triggered call_with_puma_defib"
 			raise exception
 		end
 
 		def self.included(base)
-			base.send(:alias_method, :call, :call_with_puma_defib)
 			base.send(:alias_method, :call_without_puma_defib, :call)
-			base.send(:alias_method, :render_exception, :render_exception_with_puma_defib)
+			base.send(:alias_method, :call, :call_with_puma_defib)
 			base.send(:alias_method, :render_exception_without_puma_defib, :render_exception)
+			base.send(:alias_method, :render_exception, :render_exception_with_puma_defib)
 		end
 	end
 end
