c073b02fb4e5da5482cd0f1397553c80bfef6259
Tony Ha <tonytaesung.ha@gmail.com>
2020-10-04 22:24:08 -0700

    Updated inclusion logic.

--- lib/puma_defibrillator/exception_methods.rb
+++ lib/puma_defibrillator/exception_methods.rb
@@ -1,12 +1,14 @@
 module PumaDefibrillator
 	module ExceptionMethods
-		included do
-			rescue_from Rack::Timeout::RequestTimeoutException, with: :handle_rack_timeout
+		def self.included(base)
+			base.class_eval do
+				rescue_from Rack::Timeout::RequestTimeoutException, with: :handle_rack_timeout
 
-			def handle_rack_timeout
-				puts "============================"
-	      puts "log-rack-timeout running"
-	      puts "============================"
+				def handle_rack_timeout
+					puts "============================"
+		      puts "log-rack-timeout running"
+		      puts "============================"
+				end
 			end
 		end
 	end
