f82772a68b579961e513b3f97fe4b029405906ca
Tony Ha <tonytaesung.ha@gmail.com>
2020-10-05 23:42:18 -0700

    Added logic to send off heroku dyno restart request.

--- lib/puma_defibrillator/heroku_restarter.rb
+++ lib/puma_defibrillator/heroku_restarter.rb
@@ -1,27 +1,28 @@
 module PumaDefibrillator
 	class HerokuRestarter
 		def initialize(configuration)
-			@token = configuration.heroku_api_token
-			@dynos = configuration.dyno_list
+			@api_token = configuration.heroku_api_token
+			@dyno_list = configuration.dyno_list
 			@app_name = configuration.app_name
 		end
 
 		def call
 			puts "heroku restarter#call running"
-			# @dynos.each do |dyno|
-			# 	uri = URI(dyno_url(process))
-			# 	req = Net::HTTP::Delete.new(uri.path)
-			# 	req["Authorization"] = authorization
-			# 	req["Content-type"] = "application/json"
-			# 	req["Accept"] = "application/vnd.heroku+json; version=3"
-			# 	res = Net::HTTP.start(uri.host, uri.port, use_ssl: (uri.scheme == "https")) { |http| http.request(req) }
-			# end
+			@dyno_list.each do |dyno|
+				uri = URI(dyno_url(dyno))
+				req = Net::HTTP::Delete.new(uri.path)
+				req["Authorization"] = authorization
+				req["Content-type"] = "application/json"
+				req["Accept"] = "application/vnd.heroku+json; version=3"
+				res = Net::HTTP.start(uri.host, uri.port, use_ssl: (uri.scheme == "https")) { |http| http.request(req) }
+			end
+			puts "restart request sent to heroku"
 		end
 
 		private
 
 		def dyno_url(dyno)
-			"https://api.heroku.com/apps/#{@app_name}/dynos/#{process}"
+			"https://api.heroku.com/apps/#{@app_name}/dynos/#{dyno}"
 		end
 
 		def authorization
