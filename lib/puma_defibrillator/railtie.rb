a68382f2b9b3cbb93ba367624db3494497fbd747
Tony Ha <tonytaesung.ha@gmail.com>
2020-10-11 15:27:35 -0700

    Removed railtie.

--- lib/puma_defibrillator/railtie.rb
+++
@@ -1,9 +0,0 @@
-module PumaDefibrillator
-  class Railtie < ::Rails::Railtie
-    initializer 'puma_defibrillator.middleware.rails' do |app|
-      require 'puma_defibrillator/exception_methods'
-
-      ActionController::Base.send(:include, ExceptionMethods)
-    end
-  end
-end
